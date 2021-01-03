3b5bea0e90c2befedcdbaa3044037da5092e97c4
chshr005 <chshr005@gmail.com>
2021-01-03 22:19:19 +0000

    server test

--- server-client-script.sh
+++ server-client-script.sh
@@ -2,7 +2,7 @@
 
 echo "Running script..."
 source $1
-tmux new-session -d "python3 server.py" \;
+tmux new-session -d "python3 base_server.py" \;
 sleep 20
-tmux split-window "python3 client.py" \; attach 
+tmux split-window "python3 base_server2.py" \; attach 
  
